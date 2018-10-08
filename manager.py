8fa6a70654558fceea1448cc1ad1967deb7a57ca
TuringEmmy <yonglonggeng@163.com>
2018-10-08 17:02:51 +0800

    create project fro manager.py

--- manager.py
+++ manager.py
@@ -1,4 +1,17 @@
 # author        TuringEmmy 
 # createtime    18-10-8  下午12:17
 # coding=utf-8
-# doc           PyCharm 
+# doc           PyCharm
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/index')
+def index():
+    return "index"
+
+
+if __name__ == '__main__':
+    app.run()
