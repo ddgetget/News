3d7c42ec689ee6516fe007dc326de63a77259d5a
TuringEmmy <yonglonggeng@163.com>
2018-10-12 15:58:49 +0800

    admin logo is ok with csrf_token added

--- info/__init__.py
+++ info/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 # 导入Config类，导入config_dict字典
-from config import Config,config_dict
+from config import Config, config_dict
 from flask_sqlalchemy import SQLAlchemy
 from flask_session import Session
 # 导入日志模块
@@ -9,16 +9,15 @@ import logging
 from logging.handlers import RotatingFileHandler
 from redis import StrictRedis
 
-
 # 先实例化sqlalchemy对象
 db = SQLAlchemy()
 # 实例化redis对象,用来实现存储和业务相关的数据
-redis_store = StrictRedis(host=Config.REDIS_HOST,port=Config.REDIS_PORT,decode_responses=True)
+redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT, decode_responses=True)
 
 # 设置日志的记录等级
-logging.basicConfig(level=logging.DEBUG) # 调试debug级
+logging.basicConfig(level=logging.DEBUG)  # 调试debug级
 # 创建日志记录器，指明日志保存的路径、每个日志文件的最大大小、保存的日志文件个数上限
-file_log_handler = RotatingFileHandler("logs/log", maxBytes=1024*1024*100, backupCount=10)
+file_log_handler = RotatingFileHandler("logs/log", maxBytes=1024 * 1024 * 100, backupCount=10)
 # 创建日志记录的格式 日志等级 输入日志信息的文件名 行数 日志信息
 formatter = logging.Formatter('%(levelname)s %(filename)s:%(lineno)d %(message)s')
 # 为刚创建的日志记录器设置日志记录格式
@@ -27,13 +26,11 @@ file_log_handler.setFormatter(formatter)
 logging.getLogger().addHandler(file_log_handler)
 
 # 导入flask_wtf扩展提供的csrf保护和验证功能
-from flask_wtf import CSRFProtect,csrf
-
+from flask_wtf import CSRFProtect, csrf
 
 
 # 定义工厂函数，实现动态的根据传入参数的不同，生产不同环境下的app
 def create_app(config_name):
-
     app = Flask(__name__)
 
     # 加载配置对象,接收工厂函数传入的参数
@@ -45,17 +42,17 @@ def create_app(config_name):
     Session(app)
     # 项目开启csrf保护
     CSRFProtect(app)
+
     # 生成csrf_token,通过请求钩子，在每次请求后，往客户端浏览器的cookie中设置csrf_token
     @app.after_request
     def after_request(response):
         csrf_token = csrf.generate_csrf()
-        response.set_cookie('csrf_token',csrf_token)
+        response.set_cookie('csrf_token', csrf_token)
         return response
 
     # 添加自定义过滤器给模板
     from info.utils.commons import index_filter
-    app.add_template_filter(index_filter,'index_filter')
-
+    app.add_template_filter(index_filter, 'index_filter')
 
     # 导入蓝图对象
     from info.modules.news import news_blue
@@ -71,8 +68,4 @@ def create_app(config_name):
     from info.modules.admin import admin_blue
     app.register_blueprint(admin_blue)
 
-
     return app
-
-
-
--- info/modules/admin/views.py
+++ info/modules/admin/views.py
@@ -1,4 +1,53 @@
 # author        TuringEmmy 
 # createtime    18-10-12  下午3:27
 # coding=utf-8
-# doc           PyCharm 
+# doc           PyCharm
+from flask import current_app, jsonify
+from flask import render_template, request
+from flask import session
+
+from info import db
+from info.modules.admin import admin_blue
+
+from info.models import User
+from info.utils.response_code import RET
+
+
+@admin_blue.route('/login', methods=["POST","GET"])
+def admin_login():
+    if request.method == 'GET':
+        return render_template('admin/login.html')
+    # 获取登陆的参数
+    username = request.form.get('usrename')
+    password = request.form.get('password')
+
+    # 校验3参数的完整性
+    if not all([username, password]):
+        return render_template('admin/login.html', errmsg='参数不完整')
+
+    # 查询数据库
+    try:
+        user = User.query.filter(User.mobile == username).first()
+    except Exception as e:
+        current_app.logger.error(e)
+        db.session.rollback()
+        return jsonify(errno=RET.DBERR, errmsg="数据查询失败")
+
+    # 验证数据库查询结果的完整性
+    if not user:
+        return render_template('admin/login.html', errmsg='用户不存在')
+
+    if not user.check_password(password):
+        return render_template('admin/login.html', errmsg='密码错误')
+
+    # 检查是否是管理员，只需要验证is_admin参数的1还是0
+    if not user.is_admin:
+        return render_template('admin/login.html', errmsg='用户权限错误')
+
+    # 一定要记得保存登陆后的用户信息到session
+    session['user_id'] = user.id
+    session['nick_name'] = user.nick_name
+    session['mobile'] = user.mobile
+    session['is_admin'] = user.is_admin
+
+    return render_template('admin/index.html')
