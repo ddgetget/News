d2aeaead2b6f4863da7b38376b4fc5d93dac3dc1
TuringEmmy <yonglonggeng@163.com>
2018-10-14 15:36:03 +0800

    news review is ok

--- info/modules/admin/views.py
+++ info/modules/admin/views.py
@@ -12,10 +12,11 @@ from flask import session
 from flask import redirect
 from flask import url_for
 
+from info import constants
 from info import db
 from info.modules.admin import admin_blue
 
-from info.models import User
+from info.models import User, News
 from info.utils.response_code import RET
 
 from info.utils.commons import login_required
@@ -71,6 +72,8 @@ def admin_login():
     # 跳转到后台管理主页
     return redirect(url_for('admin.admin_index'))
 
+
+# ----------------------------------用户统计模块--------------------------------------------------
 @admin_blue.route('/user_count')
 def user_count():
     """
@@ -85,10 +88,10 @@ def user_count():
     # 总人数
     total_count = 0
     try:
-        total_count = User.query.filter(User.is_admin==False).count()
+        total_count = User.query.filter(User.is_admin == False).count()
     except Exception as e:
         current_app.logger.error(e)
-        return jsonify(errno=RET.DBERR,errmsg='查询总人数失败')
+        return jsonify(errno=RET.DBERR, errmsg='查询总人数失败')
 
     # tm_year=2018, tm_mon=10, tm_mday=12
     # 2018-10-12---2018-10-01
@@ -96,52 +99,53 @@ def user_count():
     mon_count = 0
     t = time.localtime()
     # 每月开始日期的字符串:2018-10-01
-    mon_begin_date_str = '%d-%02d-01' % (t.tm_year,t.tm_mon)
+    mon_begin_date_str = '%d-%02d-01' % (t.tm_year, t.tm_mon)
     # 用户注册时间为datetime对象，要把日期字符串转成日期对象
-    mon_begin_date = datetime.strptime(mon_begin_date_str,'%Y-%m-%d')
+    mon_begin_date = datetime.strptime(mon_begin_date_str, '%Y-%m-%d')
     # 查询数据库，获取月新增人数
     try:
         # 用户注册时间大于每月1日
-        mon_count = User.query.filter(User.is_admin==False,User.create_time>mon_begin_date).count()
+        mon_count = User.query.filter(User.is_admin == False, User.create_time > mon_begin_date).count()
     except Exception as e:
         current_app.logger.error(e)
-        return jsonify(errno=RET.DBERR,errmsg='查询月新增人数失败')
+        return jsonify(errno=RET.DBERR, errmsg='查询月新增人数失败')
 
     # 日新增人数
     day_count = 0
     # 获取每天的开始时间字符串
-    day_begin_date_str = '%d-%02d-%02d' % (t.tm_year,t.tm_mon,t.tm_mday)
+    day_begin_date_str = '%d-%02d-%02d' % (t.tm_year, t.tm_mon, t.tm_mday)
     # 把日期字符串转成日期对象
-    day_begin_date = datetime.strptime(day_begin_date_str,'%Y-%m-%d')
+    day_begin_date = datetime.strptime(day_begin_date_str, '%Y-%m-%d')
     # 查询数据库，获取日新增人数
     try:
-        day_count = User.query.filter(User.is_admin==False,User.create_time>day_begin_date).count()
+        day_count = User.query.filter(User.is_admin == False, User.create_time > day_begin_date).count()
     except Exception as e:
         current_app.logger.error(e)
-        return jsonify(errno=RET.DBERR,errmsg='查询日新增人数失败')
+        return jsonify(errno=RET.DBERR, errmsg='查询日新增人数失败')
 
     # 用户活跃度统计
     active_count = []
     active_time = []
     # 获取当前日期字符串
-    today_begin_data_str = '%d-%02d-%02d' % (t.tm_year,t.tm_mon,t.tm_mday)
+    today_begin_data_str = '%d-%02d-%02d' % (t.tm_year, t.tm_mon, t.tm_mday)
     # 转换日期对象
-    today_begin_date = datetime.strptime(today_begin_data_str,'%Y-%m-%d')
+    today_begin_date = datetime.strptime(today_begin_data_str, '%Y-%m-%d')
 
     # 从当前往前推31天，获取每天的开始时间、结束时间
-    for d in range(0,31):
-        begin_date = today_begin_date - timedelta(days=d) # 每天的开始时间
-        end_date = today_begin_date - timedelta(days=(d-1)) # 每天的结束时间
+    for d in range(0, 31):
+        begin_date = today_begin_date - timedelta(days=d)  # 每天的开始时间
+        end_date = today_begin_date - timedelta(days=(d - 1))  # 每天的结束时间
         # 根据每天的日期来查询mysql数据库，比较用户的登录时间
         try:
-            count = User.query.filter(User.is_admin==False,User.last_login>=begin_date,User.last_login<end_date).count()
+            count = User.query.filter(User.is_admin == False, User.last_login >= begin_date,
+                                      User.last_login < end_date).count()
         except Exception as e:
             current_app.logger.error(e)
-            return jsonify(errno=RET.DBERR,errmsg='查询用户活跃数据失败')
+            return jsonify(errno=RET.DBERR, errmsg='查询用户活跃数据失败')
         # 添加查询结果到列表中
         active_count.append(count)
         # 把时间对象转成时间字符串
-        begin_date_str = datetime.strftime(begin_date,'%Y-%m-%d')
+        begin_date_str = datetime.strftime(begin_date, '%Y-%m-%d')
         active_time.append(begin_date_str)
 
     active_time.reverse()
@@ -149,13 +153,13 @@ def user_count():
 
     # 定义字典
     data = {
-        'total_count':total_count,
-        'mon_count':mon_count,
-        'day_count':day_count,
-        'active_count':active_count,
-        'active_time':active_time
+        'total_count': total_count,
+        'mon_count': mon_count,
+        'day_count': day_count,
+        'active_count': active_count,
+        'active_time': active_time
     }
-    return render_template('admin/user_count.html',data=data)
+    return render_template('admin/user_count.html', data=data)
 
 
 # ------------------------------------------后台管理主页--------------------------------------
@@ -168,71 +172,98 @@ def admin_index():
     return render_template('admin/index.html', user=user.to_dict())
 
 
-# --------------------------用户统计-------------------------------------------
-# @admin_blue.route('/user_count')
-# def user_count():
-#     # 查询总人数
-#     total_count = 0
-#     try:
-#         total_count = User.query.filter(User.is_admin == False).count()
-#     except Exception as e:
-#         current_app.logger.error(e)
-#         db.session.rollback()
-#
-#     # 查询约新增人人数
-#     mon_count = 0
-#     try:
-#         now = time.localtime()
-#         mon_begin = '%d-%02d-01' % (now.tm_year, now.tm_mon)
-#         mon_begin_date = datetime.strptime(mon_begin, '%Y-%m-%d')
-#         mon_count = User.query.filter(User.is_admin == False, User.create_time >= mon_begin_date).count()
-#     except Exception as e:
-#         current_app.logger.error(e)
-#
-#         # 查询日新增人数
-#     day_count = 0
-#     try:
-#         day_begin = '%d-%02d-%02d' % (now.tm_year, now.tm_mon, now.tm_mday)
-#         day_begin_date = datetime.strptime(day_begin, '%Y-%m-%d')
-#         day_count = User.query.filter(User.is_admin == False, User.create_time >= day_begin_date).count()
-#     except Exception as e:
-#         current_app.logger.error(e)
-#         db.session.rollback()
-#         return jsonify(errno=RET.DBERR, errmsg='保存数据失败')
-#
-#     now_date = datetime.strptime(datetime.now().strftime("%Y-%m-%d"), "%Y-%m-%d")
-#
-#     active_time = []
-#     active_count = []
-#
-#     for i in range(0, 31):
-#         begin_date = now_date - timedelta(days=i)
-#         end_date = now_date - timedelta(days=(i - 1))
-#
-#         active_time.append((begin_date.strftime('%Y-%m-%d')))
-#         count = 0
-#         try:
-#             count = User.query.filter(User.is_admin == False, User.last_login >= day_begin,
-#                                       User.last_login < end_date).count()
-#         except Exception as e:
-#             current_app.logger.error(e)
-#             db.session.rollback()
-#             return jsonify(errno=RET.DBERR, errmsg='保存数据失败')
-#
-#         active_count.append(count)
-#         begin_date_str = datetime.strftime(begin_date,'%Y-%m-%d')
-#         active_count.append(begin_date_str)
-#
-#
-#     active_time.reverse()
-#     active_count.reverse()
-#
-#     data = {
-#         'total_count': total_count,
-#         'mon_count': mon_count,
-#         'day_count': day_count,
-#         'active_time': active_time,
-#         'active_count': active_count
-#     }
-#
-#     return render_template('admin/user_count.html', data=data)
+# -------------------------------用户列表-------------------------------------------------------
+
+@admin_blue.route('/user_list')
+@login_required
+def user_list():
+    """
+    获取用户列表
+    :return:
+    """
+    # 获取参数
+
+    page = request.args.get("p", 1)
+
+    try:
+        page = int(page)
+    except Exception as e:
+        current_app.logger.error(e)
+        page = 1
+
+    # 设置默认值
+    users = []
+    current_page = 1
+    total_page = 1
+
+    # 查询数据
+    try:
+        paginate = User.query.filter(
+            User.is_admin == False
+        ).order_by(
+            User.last_login.desc()
+        ).paginate(page, constants.ADMIN_USER_PAGE_MAX_COUNT, False)
+        users = paginate.items
+        current_page = paginate.page
+        total_page = paginate.pages
+    except Exception as e:
+        current_app.logger.error(e)
+        # print(e)
+        # print('sadas')
+        return jsonify(errno=RET.NODATA, errmsg='data is error')
+    # 将模型列表转化成走字典列表
+
+    users_list = []
+    for user in users:
+        users_list.append(user.to_admin_dict())
+
+    context = {
+        'total_page': total_page,
+        "current_page": current_page,
+        'users': users_list
+    }
+
+    return render_template('admin/user_list.html', context=context)
+
+
+# --------------------------------新闻审核-------------------------------------------------------
+@admin_blue.route('/news_review')
+def news_review():
+    """
+    返回待审核新闻列表
+    :return:
+    """
+
+    page = request.args.get('p', 1)
+
+    try:
+        page = int(page)
+    except Exception as e:
+        current_app.logger.error(e)
+        page = 1
+
+    news_list = []
+    current_page = 1
+    total_page = 1
+
+    try:
+        paginate = News.query.filter(
+            News.status != 0
+        ).order_by(
+            News.create_time.desc()
+        ).paginate(page, constants.ADMIN_NEWS_PAGE_MAX_COUNT, False)
+
+        news_list = paginate.items
+        current_page = paginate.page
+        total_page = paginate.pages
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg='数据查询失败')
+
+    data = {
+        'total_page': total_page,
+        "current_page": current_page,
+        "news_list": news_list
+    }
+
+    return render_template('admin/news_review.html', data=data)
